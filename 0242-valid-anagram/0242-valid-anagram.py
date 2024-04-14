834717e971ec2a73fd258289f756b38e908dc3e0
Loveena Ramchandani <64587823+Loveena28@users.noreply.github.com>
2024-04-14 22:19:18 +0530

    Time: 58 ms (16.82%), Space: 17.7 MB (15.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,5 @@
 from collections import deque
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        letters = deque()
-        for i in s:
-            letters.append(i)
-        for j in t:
-            if j not in letters:
-                return False
-            letters.remove(j)
-        return len(letters) == 0
+        return sorted(s) == sorted(t)
             
